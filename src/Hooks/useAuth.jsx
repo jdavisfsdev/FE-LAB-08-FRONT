d450715cb0eec4110ba9379bb0a130b265fd9465
Joseph Davis <joethebartenderpdx@gmail.com>
2021-08-08 12:34:10 -0700

    redirect, components created

---
+++ src/Hooks/useAuth.jsx
@@ -0,0 +1,18 @@
+// import React, { useState, useEffect } from 'react';
+
+// export default function useAuth() {
+//   const [signUp, setSignUp] = useState(true);
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [error, setError] = useState('');
+
+//   useEffect(() => {
+
+//   })
+
+//   return (
+//     <div>
+            
+//     </div>
+//   );
+// }
